d90d9098a934838869055a571e8e2ff7d941288e
chloesacks <chloe.sacks@kellogg.northwestern.edu>
2020-06-11 04:15:36 +0000

    Commit before Heroku

--- createdb.rb
+++ createdb.rb
@@ -15,21 +15,26 @@ end
 DB.create_table! :rsvps do
   primary_key :id
   foreign_key :event_id
+  foreign_key :user_id
   Boolean :going
+  String :comments, text: true
+end
+DB.create_table! :users do
+  primary_key :id
   String :name
   String :email
-  String :comments, text: true
+  String :password
 end
 
 # Insert initial (seed) data
 events_table = DB.from(:events)
 
-events_table.insert(title: "Bacon Burger Taco Fest", 
-                    description: "Here we go again bacon burger taco fans, another Bacon Burger Taco Fest is here!",
-                    date: "June 21",
-                    location: "Kellogg Global Hub")
+events_table.insert(title: "Section Picnic", 
+                    description: "BYOE(verything) to this socially-distanced picnic!! We will find a large space on the lawn and set up our picnic blankets at least six feet away from each other.",
+                    date: "June 15",
+                    location: "Deering Lawn")
 
-events_table.insert(title: "Kaleapolooza", 
-                    description: "If you're into nutrition and vitamins and stuff, this is the event for you.",
-                    date: "July 4",
-                    location: "Nowhere")
+events_table.insert(title: "Beach Bonfire", 
+                    description: "Let's celebrate (outdoors) our last night before we graduate--where else but the beach?!",
+                    date: "June 18",
+                    location: "Lincoln St. Beach")
